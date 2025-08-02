cb75e46bf660c0589b56ca34cac2e94a330fc282
mehdighasemi22 <ghmatiyo@gmail.com>
2025-08-02 13:25:54 -0700

    Create for_in_range.py

---
+++ for_in_range.py
@@ -0,0 +1,22 @@
+numbers = []
+
+for i in range(5):
+    num = int(input(f"عدد {i+1} را وارد کن: "))
+    numbers.append(num)
+                                    # ravesh behine
+max_number = max(numbers)
+print("بزرگ‌ترین عدد:", max_number)
+
+
+# =========================================================================
+
+# shomare1 = int(input("adad aval: "))
+# shomare2 = int(input("adad dovom: "))
+# shomare3 = int(input("adad sevom: "))
+# shomare4 = int(input("adad chaharom: "))
+# shomare5 = int(input("adad panjom: "))
+                                                                
+# numbers = [shomare1, shomare2, shomare3, shomare4, shomare5]
+
+# max_number = max(numbers)
+# print("the biggest number is ", max_number)
